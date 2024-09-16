add540ab24bf804492e63ac2f4daa0a663b3b712
Argenis Gonzalez <argenis.gonzalez@theksquaregroup.com>
2024-09-16 13:51:30 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom"; // Asegúrate de importar BrowserRouter
 import {
   mainBody,
   about,
@@ -13,11 +14,8 @@ import AboutMe from "./components/home/AboutMe";
 import Project from "./components/home/Project";
 import Footer from "./components/Footer";
 import Skills from "./components/home/Skills";
-// import { Blog } from "./components/blog/Blog";
-// import BlogPost from "./components/blog/BlogPost";
 import GetInTouch from "./components/home/GetInTouch.jsx";
 import Leadership from "./components/home/Leadership.jsx";
-
 import Experience from "./components/home/Experience";
 
 const Home = React.forwardRef((props, ref) => {
@@ -70,18 +68,22 @@ const Home = React.forwardRef((props, ref) => {
 
 const App = () => {
   return (
-    <div>
-      <Home></Home>
-      <Footer>
-        {getInTouch.show && (
-          <GetInTouch
-            heading={getInTouch.heading}
-            message={getInTouch.message}
-            email={getInTouch.email}
-          />
-        )}
-      </Footer>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      {" "}
+      {/* Asegúrate de añadir esto */}
+      <div>
+        <Home></Home>
+        <Footer>
+          {getInTouch.show && (
+            <GetInTouch
+              heading={getInTouch.heading}
+              message={getInTouch.message}
+              email={getInTouch.email}
+            />
+          )}
+        </Footer>
+      </div>
+    </Router>
   );
 };
 
